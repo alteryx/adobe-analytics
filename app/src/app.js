2cb96640d5e6e12a8e5f1fe72ff6b707e8705952
Stephen Wagner <stephen.wagner.a@gmail.com>
2017-04-04 13:01:00 -0500

    US24520 Report.Validate - Continuously update store.reportDescription

--- app/src/app.js
+++ app/src/app.js
@@ -129,6 +129,11 @@ Alteryx.Gui.AfterLoad = (manager) => {
     return value !== ''
   }
 
+  // continuously updates the reportDescription each time a selection is made
+  autorun(() => {
+    store.reportDescription = JSON.stringify(reportValidation.payload(store))
+  })
+
   // Checks the state of advOptions and displays the correct arrow image
   autorun(() => {
     store.advOptionsPrimary === true ? document.getElementById('primaryArrow').className = 'arrow-down' : document.getElementById('primaryArrow').className = 'arrow-right'
@@ -219,7 +224,7 @@ Alteryx.Gui.AfterLoad = (manager) => {
     }
   })
 
-// Determines whether to show/hide the loading spinner based on page
+  // Determines whether to show/hide the loading spinner based on page
   autorun(() => {
     // Shows or hides the loading spinner based on flag
     const loading = (flag) => {
